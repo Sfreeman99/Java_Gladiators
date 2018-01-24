f479e28994d4f0ddbd5e157c5bc75831862a021d
shedliafreeman <shedliafreeman@basecampcodingacademy.org>
2018-01-24 16:07:56 -0600

    Added Test that makes sure that a gladiator can attack another gladiator which will make the attacked gladiator loose health. This test is currently failing.

--- Test/com/company/GladiatorTest.java
+++ Test/com/company/GladiatorTest.java
@@ -10,5 +10,23 @@ class GladiatorTest {
         Gladiator playerOne = new Gladiator("Shedlia Freeman");
         assertEquals("Shedlia Freeman", playerOne.name);
     }
+    @Test
+    void makeTwoGladiators() {
+        Gladiator playerOne;
+        Gladiator playerTwo;
+        playerOne = new Gladiator("Bob");
+        playerTwo = new Gladiator("Billy");
+        assertEquals("Bob", playerOne.name);
+        assertEquals("Billy", playerTwo.name);
+    }
+    @Test
+    void TestGladiatorCanAttackOtherGladiator() {
+        Gladiator playerOne;
+        Gladiator playerTwo;
+        playerOne = new Gladiator("Billy");
+        playerTwo = new Gladiator("Tom");
+        playerOne.attack(playerTwo);
+        assertTrue(playerTwo.health < 100);
+    }
 
 }
