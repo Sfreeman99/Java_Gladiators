8a26501fadbe62a3e57236a686ece175f95c7b6e
shedliafreeman <shedliafreeman@basecampcodingacademy.org>
2018-01-24 15:46:25 -0600

    Wrote minimal code so that the test for makeAGladiator test would work.

--- src/com/company/Gladiator.java
+++ src/com/company/Gladiator.java
@@ -1,4 +1,22 @@
 package com.company;
 
 public class Gladiator {
+
+    public String name;
+    public int attackLow;
+    public int attackHigh;
+    public int health;
+    public int rage;
+
+    public Gladiator(String playerName) {
+        /*
+        this is the gladiator constructor which is called when you want a new gladiator object
+        this object has a name, attackLow, attackHigh, health, and rage
+        */
+        name = playerName;
+        attackLow = 0;
+        attackHigh = 10;
+        health = 100;
+        rage = 0;
+    }
 }
