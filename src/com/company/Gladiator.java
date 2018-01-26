0cc3e9d1743b3faec6f09a4f2f7e55852a6057e7
shedliafreeman <shedliafreeman@basecampcodingacademy.org>
2018-01-26 11:18:57 -0600

    Wrote Minimal code so that the test "TestGladiatorCanHeal" would work.

--- src/com/company/Gladiator.java
+++ src/com/company/Gladiator.java
@@ -1,10 +1,12 @@
 package com.company;
 
+
+
 public class Gladiator {
 
     public String name;
-    public int attackLow;
-    public int attackHigh;
+    public double attackLow;
+    public double attackHigh;
     public int health;
     public int rage;
 
@@ -14,9 +16,21 @@ public class Gladiator {
         this object has a name, attackLow, attackHigh, health, and rage
         */
         name = playerName;
-        attackLow = 0;
-        attackHigh = 10;
+        attackHigh = 25;
+        attackLow = 10;
         health = 100;
         rage = 0;
+    }
+    public int attack(Gladiator defender){
+        double attack = Math.random() * attackHigh + attackLow;
+        defender.health -= attack;
+        return defender.health;
+    }
+    public double heal() {
+        this.health += 15;
+        if (this.health > 100)
+            return health = 100;
+        else
+            return health;
     }
 }
